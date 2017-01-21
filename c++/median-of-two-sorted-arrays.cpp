6f37b6e3757ce5f90e1f10f025becfc261e02c2a
Andrew Wong <argsno@gmail.com>
2017-01-21 11:34:19 +0800

    4. Median of Two Sorted Arrays

---
+++ c++/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    int length = nums1.size() + nums2.size();
+    if (length & 1)
+      return findKth(vector<int>& nums1, vector<int>& nums2, 0, 0, length/2 + 1);
+    else
+      return (findKth(vector<int>& nums1, vector<int>& nums2, 0, 0, length/2) +
+              findKth(vector<int>& nums1, vector<int>& nums2, 0, 0, length/2 + 1)) / 2.0;
+  }
+
+  double findKth(vector<int>& A, vector<int>& B, int A_st, int B_st, int k) {
+    if (A_st >= A.size())
+      return B[B_st + k - 1];
+    if (B_st >= B.size())
+      return A[A_st + k - 1];
+    if (k == 1)
+      return min(A[A_st], B[B_st]);
+    int A_key = A_st + k/2 - 1 >= A.size() ? INT_MAX : A[A_st + k/2 - 1];
+    int B_key = B_st + k/2 - 1 >= B.size() ? INT_MAX : B[B_st + k/2 - 1];
+    if (A_key < B_key)
+      return findKth(A, B, A_st + k/2, B_st, k - k/2);
+    else
+      return findKth(A, B, A_st, B_st + k/2, k - k/2);
+  }
+};
